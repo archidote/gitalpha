c5f8e7a61a95f12f54f76a038b933438678df08b
archidote <archidote.contact@gmail.com>
2022-10-28 20:27:32 +0200

    3e commit

--- test.py
+++ test.py
@@ -4,4 +4,4 @@ ip="x.x.x.x"
 username="xxxxxxx"
 ssh = paramiko.SSHClient()
 ssh.connect(ip, username=username)
-ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command("echo 123 > /tmp/test.txt")
+ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command("echo 'hello world' > /tmp/hello-world.txt")
