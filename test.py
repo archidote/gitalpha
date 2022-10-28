57700ebf2e1976d836ab2120818f811340ed785c
archidote <archidote.contact@gmail.com>
2022-10-28 20:25:50 +0200

    second  commit

---
+++ test.py
@@ -0,0 +1,7 @@
+import paramiko
+
+ip="x.x.x.x"
+username="xxxxxxx"
+ssh = paramiko.SSHClient()
+ssh.connect(ip, username=username)
+ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command("echo 123 > /tmp/test.txt")
