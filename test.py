ae20d78a6c79e96009ac95feab305f9df00bcc9f
archidote <archidote.contact@gmail.com>
2022-11-07 20:20:21 +0100

    final

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 import paramiko
 
 ip="x.x.x.x"
-username="xxxxxxx"
-password="xxxxxxx"
+username="archidote"
+password="123+aze"
 ssh = paramiko.SSHClient()
 ssh.connect(ip, username=username)
 # ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command("echo 'hello world' > /tmp/hello-world.txt")
