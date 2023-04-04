bda3ec453c782743fa3377032ba4635a66040bc9
archidote <archidote.contact@gmail.com>
2023-04-04 10:14:01 +0200

    1st year trainee

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
 import paramiko
 
-ip="x.x.x.x"
+ip="192.168.5.3"
 username="archidote"
 password="123+aze"
+api_token="dsgfsg546ds4gf65fds4g5fd4s5dgdgf51fdg545q"
+path="/var/www/html"
 test = "test"
 ssh = paramiko.SSHClient()
 ssh.connect(ip, username=username)
